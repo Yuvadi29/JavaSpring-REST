3c84920dc987262015f2195b139fe7e1e33ee959
Yuvadi29 <adityatrivedi612@gmail.com>
2024-02-14 21:04:29 +0530

    Routes Created

---
+++ src/main/java/com/example/RestApi/Routes/DataRoutes.java
@@ -0,0 +1,21 @@
+package com.example.RestApi.Routes;
+
+import com.example.RestApi.model.Data;
+
+import java.util.List;
+
+public interface DataRoutes {
+    // Create
+    Data createData(Data data);
+
+    // Read
+    List<Data> getAllData(Data data);
+
+    Data getAllDataById(long productId);
+
+    // Update
+    Data updateData(Data data);
+
+    // Delete
+    void deleteData(long id);
+}
